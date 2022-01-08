425300c82ede2a04789c933ea0192fa27516e316
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-08 14:45:19 +0530

    Fech API and Shown data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,110 @@
+import React, { Component } from 'react'
 import './App.css';
 import "weather-icons-master/css/weather-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Weather from './components/Weather';
+import Form from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+// api call api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
+const API_Key = "d7d3c2ff1cd71ecaf9f2e4eaa8a04065"
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      temp_celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false,
+    }
+
+    this.getWeather();
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
+    }
+  }
+
+  // function to convert f to celsius
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  getWeatherIcon(icons, rangeID) {
+    switch (true) {
+      case rangeID >= 200 && rangeID <= 232:
+        this.setState({ icon: this.weatherIcon.Thunderstorm });
+        break;
+      case rangeID >= 300 && rangeID <= 321:
+        this.setState({ icon: this.weatherIcon.Drizzle });
+        break;
+      case rangeID >= 500 && rangeID <= 531:
+        this.setState({ icon: this.weatherIcon.Rain });
+        break;
+      case rangeID >= 600 && rangeID <= 622:
+        this.setState({ icon: this.weatherIcon.Snow });
+        break;
+      case rangeID >= 701 && rangeID <= 781:
+        this.setState({ icon: this.weatherIcon.Atmosphere });
+        break;
+      case rangeID >= 800:
+        this.setState({ icon: this.weatherIcon.Clear });
+        break;
+      case rangeID >= 801 && rangeID <= 804:
+        this.setState({ icon: this.weatherIcon.Clouds });
+        break;
+      default:
+        this.setState({ icon: this.weatherIcon.Clear });
+    }
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_Key}`)
+
+    const response = await api_call.json();
+
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      temp_celsius: this.calCelsius(response.main.temp),
+      temp_min: this.calCelsius(response.main.temp_min),
+      temp_max: this.calCelsius(response.main.temp_max),
+      description: response.weather[0].description
+    });
+
+    this.getWeatherIcon(this.weatherIcon, response.weather[0].id);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temp_celsius={this.state.temp_celsius}
+          temp_min={this.state.temp_min}
+          temp_max={this.state.temp_max}
+          description={this.state.description}
+          weatherIcon={this.state.icon} />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-const Weather = () => {
+const Weather = (props) => {
     return (
         <div className='container'>
             <div className="cards">
-                <h1>London</h1>
+                <h1>{props.city}, {props.country}</h1>
                 <h5 className="py-4">
-                    <i className="wi wi-day-sunny display-1" />
+                    <i className={`wi ${props.weatherIcon} display-1`} />
                 </h5>
 
-                <h1 className='py-2'>21&deg;</h1>
+                <h1 className='py-2'>{props.temp_celsius}&deg;</h1>
 
                 {/* show min and max temp */}
-                {minmaxTemp(25,38)}
+                {minmaxTemp(props.temp_min, props.temp_max)}
 
-                <h4 className="py-3">Slow Rain</h4>
+                <h4 className="py-3">{props.description}</h4>
             </div>
         </div>
     )
 
 
-    function minmaxTemp(min,max){
+    function minmaxTemp(min, max) {
         return (
             <h3>
                 <span className="px-4">{min}&deg;</span>
